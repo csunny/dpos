0463902d1e9eb48c4ef18815ba8040b9a953c9c6
csunny <cfqcsunny@gmail.com>
2018-09-07 19:10:53 +0800

    cmd: add command for vote

--- tools/vote.go
+++ tools/vote.go
@@ -5,35 +5,58 @@ This file is created by magic at 2018-9-3
 package tools
 
 import (
-	"flag"
-	"log"
 	"fmt"
 	"os"
 	"io/ioutil"
 	"strings"
 	"strconv"
+	"github.com/urfave/cli"
+	"github.com/outbrain/golib/log"
 	"github.com/csunny/argo/src/dpos"
 
 )
 
+// NodeVote 节点投票命令
+var NodeVote = cli.Command{
+	Name: "vote",
+	Usage: "vote for node",
+	Flags: []cli.Flag{
+		cli.StringFlag{
+			Name: "name",
+			Value: "",
+			Usage: "节点名称",
+		},
+		cli.IntFlag{
+			Name: "v",
+			Value: 0,
+			Usage: "投票数量",
+		},
+	}, 
+	Action: func(context *cli.Context) error{
+		if err := Vote(context); err != nil{
+			return err
+		} 
+		return nil
+	},
+}
 // Vote for node. The votes of node is origin vote plus new vote.
 // votes = originVote + vote 
-func Vote() {
-	name := flag.String("name", "", "节点名称")
-	vote := flag.Int("v", 0, "投票数量")
-	flag.Parse()
+func Vote(context *cli.Context) error {
+	name := context.String("name")
+	vote := context.Int("v")
 
-	if *name == "" {
-		log.Fatal("节点名称不能为空")
+	if name == "" {
+		log.Errorf("节点名称不能为空")
 	}
 
-	if *vote < 1 {
-		log.Fatal("最小投票数目为1")
+	if vote < 1 {
+		log.Errorf("最小投票数目为1")
 	}
 
 	f, err := ioutil.ReadFile(dpos.FileName)
 	if err != nil {
-		log.Fatal(err)
+		log.Errorf(err.Error())
+		return err
 	}
 	res := strings.Split(string(f), "\n")
 
@@ -45,19 +68,25 @@ func Vote() {
 		}
 	}
 
-	originVote, err := strconv.Atoi(voteMap[*name])
+	originVote, err := strconv.Atoi(voteMap[name])
 	if err != nil {
-		log.Fatal(err)
+		log.Errorf(err.Error())
+		return err
 	}
-	votes := originVote + *vote
-	voteMap[*name] = fmt.Sprintf("%d", votes)
+	votes := originVote + vote
+	voteMap[name] = fmt.Sprintf("%d", votes)
 
-	log.Printf("节点%s新增票数%d", *name, vote)
+	log.Infof("节点%s新增票数%d", name, vote)
 	str := ""
 	for k, v := range voteMap {
 		str += k + ":" + v + "\n"
 	}
 
 	file, err := os.OpenFile(dpos.FileName, os.O_RDWR, 0666)
+	if err != nil{
+		return err
+	}
 	file.WriteString(str)
+
+	return nil
 }
