2b7484afe35564d78cdd15bb612e429606598e85
csunny <cfqcsunny@gmail.com>
2018-09-07 19:10:10 +0800

    improve: update vote fun

--- main/dpos.go
+++ main/dpos.go
@@ -1,7 +1,56 @@
 package main
 
-import "github.com/csunny/dpos"
+import (
+	"os"
+	"github.com/outbrain/golib/log"
+	"github.com/urfave/cli"
+	"github.com/csunny/dpos"
+	"github.com/csunny/dpos/tools"
+)
+
+const (
+	// Version 定义
+	Version = "v0.1.0"
+)
+
+var mainFlags = []cli.Flag{
+	cli.StringFlag{
+		Name: "log_level",
+		Value: "INFO",
+		Usage: "set the log level to INFO",
+	},
+}
+
+var mainCommands = []cli.Command{
+	tools.NodeVote,
+	dpos.NewNode,
+	
+}
 
 func main()  {
-	dpos.Run()
+	app := cli.NewApp()
+	app.Name = "dpos"
+	app.Version = Version
+	app.Author = "Magic"
+	app.Flags = mainFlags
+	app.Usage = "a simple DPoS algorithm implement"
+	app.Commands = mainCommands
+	app.Before = func(context *cli.Context) error {
+		if context.GlobalBool("version") {
+			log.Info(Version)
+			os.Exit(0)
+		}
+		logLevel,err := log.LogLevelFromString(context.GlobalString("log_level"))
+		if err != nil {
+			log.Errorf("unknown log level:%s",err.Error())
+		} else {
+			log.SetLevel(logLevel)
+		}
+		return nil
+	}
+
+	err := app.Run(os.Args)
+	if err != nil{
+		log.Errorf(err.Error())		
+	}
 }
--- main/vote.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/csunny/dpos/tools"
-)
-// TODO rewite command with cli package and fix this.
-func main(){
-	tools.Vote()
-} 
